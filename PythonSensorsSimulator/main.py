837db25899f47a88c467065251b6105edd28123c
kara <ssctonioloriccardo@gmail.com>
2023-11-17 17:40:09 +0100

    Applicata la convenzione ai nomi di funzioni e variabili + aggiunte variabili private

--- PythonSensorsSimulator/main.py
+++ PythonSensorsSimulator/main.py
@@ -3,8 +3,8 @@ import time
 from Model.Writers.StdoutWriter import StdoutWriter
 from Model.BuilderSimulatorExecutor import BuilderSimulatorExecutor
 
-#Uso generale di una interfaccia Writer al fine di poter implementare quante politiche diverse di writing si vuole,
-#senza dover cambiare nulla sul resto del codice.
+# Uso generale di una interfaccia Writer al fine di poter implementare quante politiche diverse di writing si vuole,
+# senza dover cambiare nulla sul resto del codice.
 writeToStd = StdoutWriter()
 
 symExecBuilder = BuilderSimulatorExecutor(writeToStd)
@@ -12,14 +12,14 @@ symExecBuilder = BuilderSimulatorExecutor(writeToStd)
 # Builder pattern per la configurazione dell'esecutore di simulatori.
 symExec = (
     symExecBuilder
-    .addTemperatureSimulator("#1",0.5)
-    .addTemperatureSimulator("#2",0.75)
-    .addTemperatureSimulator("#3",1.5)
-    .getSimulatorExecutor()
+    .add_temperature_simulator("#1", 0.5)
+    .add_temperature_simulator("#2", 0.75)
+    .add_temperature_simulator("#3", 1.5)
+    .get_simulator_executor()
 )
 
 symExec.run_all()
-time.sleep(3.25) # do un tempo di 2 secondi per far si che si fermi da solo.
+time.sleep(3.25)  # do un tempo di 2 secondi per far si che si fermi da solo.
 symExec.stop_all()
 
-#NOTA: ereditarietà usata solamente per l'implementazione di metodi di classi astratte.
+# NOTA: ereditarietà usata solamente per l'implementazione di metodi di classi astratte.
