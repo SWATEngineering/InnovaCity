bcd356048e8ddca9464c78baf9b6f43ac044b8f7
kara <ssctonioloriccardo@gmail.com>
2023-11-21 15:34:54 +0100

    Aggiunta immagine docker + docker-compose + envvar

--- PythonSensorsSimulator/main.py
+++ PythonSensorsSimulator/main.py
@@ -1,4 +1,8 @@
 import time
+import os
+
+KAFKA_HOST = os.environ.get("KAFKA_HOST", "localhost")
+KAFKA_PORT = os.environ.get("KAFKA_PORT", "9092")
 
 from Model.BuilderSimulatorExecutor import BuilderSimulatorExecutor
 from Model.Writers.KafkaWriter import KafkaWriter
@@ -6,7 +10,7 @@ from Model.Writers.KafkaWriter import KafkaWriter
 # Uso generale di una interfaccia Writer al fine di poter implementare quante politiche diverse di writing si vuole,
 # senza dover cambiare nulla sul resto del codice.
 # writeToStd = StdoutWriter()
-writeToKafka = KafkaWriter("temperature", "localhost", "9092")
+writeToKafka = KafkaWriter("temperature", KAFKA_HOST, KAFKA_PORT)
 
 symExecBuilder = BuilderSimulatorExecutor(writeToKafka)
 
