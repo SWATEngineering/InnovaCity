0fb6770596343bc0e56fce97c70f94dfdb7c047a
Matteo Rango <rango.matteo@icloud.com>
2023-11-28 14:49:12 +0100

    Connessione clickhouse kafka (#21)

---
+++ InitDB/temperature.sql
@@ -0,0 +1,17 @@
+CREATE TABLE innovacity.temperatures_queue (
+    timestamp Float64,
+    value Float32,
+    type String,
+    id String
+) ENGINE = Kafka('kafka:9092', 'temperature', 'ch_group_1', 'JSONEachRow');
+
+CREATE TABLE innovacity.temperatures (
+    timestamp Float64,
+    value Float32,
+    type String,
+    id String
+) ENGINE = MergeTree()
+ORDER BY (id, timestamp);
+
+CREATE MATERIALIZED VIEW consumer TO innovacity.temperatures
+AS SELECT * FROM innovacity.temperatures_queue
