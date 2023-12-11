c196872c1517a403102401cc021cca5fc24452da
Dovid308 <giacomodovidio@gmail.com>
2023-12-11 21:40:49 +0100

    modificato db che da origine al grafico del secondo pannello

--- InitDB/temperature.sql
+++ InitDB/temperature.sql
@@ -20,25 +20,48 @@ ORDER BY (nome_sensore, timestamp);
 CREATE MATERIALIZED VIEW consumer TO innovacity.temperatures
 AS SELECT * FROM innovacity.temperatures_queue;
 
-CREATE TABLE innovacity.temperatures5m
+---CREATE TABLE innovacity.temperatures5m
+---(
+---    nome_sensore String,
+---    timestamp5m DATETIME64,
+---    avgTemperature AggregateFunction(avg, Float32),
+---    latitude Float64,
+---    longitude Float64
+---)
+---ENGINE = AggregatingMergeTree
+---ORDER BY (timestamp5m, nome_sensore, longitude, latitude);
+
+--CREATE MATERIALIZED VIEW innovacity.temperatures5m_mv
+--TO innovacity.temperatures5m
+--AS
+--SELECT
+--    toStartOfFiveMinutes(timestamp) AS timestamp5m,
+--    nome_sensore,
+--    avgState(value) as avgTemperature,
+--    latitude,
+--    longitude
+--FROM innovacity.temperatures
+--GROUP BY (timestamp5m, nome_sensore, latitude, longitude);
+
+CREATE TABLE innovacity.temperatures1m
 (
     nome_sensore String,
-    timestamp5m DATETIME64,
-    avgTemperature AggregateFunction(avg, Float32),
+    timestamp1m DATETIME64,
+    avgTemperature AggregateFunction(avgState, Float32), -- Using avgState for stateful aggregation
     latitude Float64,
     longitude Float64
 )
 ENGINE = AggregatingMergeTree
-ORDER BY (timestamp5m, nome_sensore, longitude, latitude);
+ORDER BY (timestamp1m, nome_sensore, longitude, latitude);
 
-CREATE MATERIALIZED VIEW innovacity.temperatures5m_mv
-TO innovacity.temperatures5m
+CREATE MATERIALIZED VIEW innovacity.temperatures1m_mv
+TO innovacity.temperatures1m
 AS
 SELECT
-    toStartOfFiveMinutes(timestamp) AS timestamp5m,
+    toStartOfMinute(timestamp) AS timestamp1m,
     nome_sensore,
     avgState(value) as avgTemperature,
     latitude,
     longitude
 FROM innovacity.temperatures
-GROUP BY (timestamp5m, nome_sensore, latitude, longitude);
+GROUP BY (timestamp1m, nome_sensore, latitude, longitude);
