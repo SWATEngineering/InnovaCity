b501fd6e29542fa327bac6d2f2ab800f7f11a1c6
Dovid308 <giacomodovidio@gmail.com>
2023-12-11 21:10:55 +0100

    ora i dati effettivamente  generati si "attaccano meglio", si perde giusto un secondo", forse ho anche idea di come sistemarlo.

--- PythonSensorsSimulator/Model/Simulators/TemperatureSimulator.py
+++ PythonSensorsSimulator/Model/Simulators/TemperatureSimulator.py
@@ -12,10 +12,7 @@ from ..Writers import Writer
 
 class TemperatureSimulator(Simulator):
     __count = 0
-    ##
     __calibration = None
-    __initial_timestamp = None
-    ##
 
     def __init__(self, writer: Writer, latitude: float, longitude: float, frequency_in_s: int = 1):
         TemperatureSimulator.__count += 1
@@ -23,28 +20,25 @@ class TemperatureSimulator(Simulator):
         # fattore di calibrazione necessario a garantire un maggiore varianza nei dati simulati dai sensori
         super().__init__(writer, latitude, longitude,
                          f"Sensore di Temperatura {TemperatureSimulator.__count}", frequency_in_s)
-        self.insert_not_real_time_data()
-    ##
 
     def get_calibration(self) -> float:
         return self.__calibration
 
     def insert_not_real_time_data(self) -> None:
-        iter_timestamp = datetime.timestamp(datetime.now())
-        primo_timestamp = iter_timestamp - 86400
+
+        last_timestamp = datetime.timestamp(
+            datetime.now()) + 20 * self.get_frequency_is_s()
+        iter_timestamp = last_timestamp
+        first_timestamp = last_timestamp - 86400
+
         data_to_insert = []
 
-        while (iter_timestamp > primo_timestamp):
+        while (iter_timestamp > first_timestamp):
             iter_timestamp -= super().get_frequency_is_s()
             hours = (iter_timestamp % 86400) / 3600
-            sym_temperature = ((
-                math.cos(
-                    math.pi * ((hours - 12) / 12)
-                    # in questo modo il periodo [-1,1] basta per raggiungere i periodi
-                ) + 1) / 2
-                # in questo modo spostiamo il coseno tutto in positivo, con valori che vanno da 0 a 1
-            ) * 12 + 5 + self.get_calibration() + random.random()
-            # per temperature che vanno da 5 a 17 gradi a fronte della fottore di calibrazione
+            sym_temperature = ((math.cos(math.pi * ((hours - 12) / 12)) + 1) / 2) * \
+                12 + 5 + self.get_calibration() + random.random()
+
             dato = {
                 "timestamp": str(datetime.fromtimestamp(iter_timestamp)),
                 "value": "{:.2f}".format(sym_temperature),
@@ -56,12 +50,17 @@ class TemperatureSimulator(Simulator):
 
             data_to_insert.append(dato)
 
-        batch_size = 1000
+        batch_size = 5000
         for i in range(0, len(data_to_insert), batch_size):
             batch = data_to_insert[i:i + batch_size]
             super().get_writer().write(json.dumps(batch))
+        time.sleep(max(0, (last_timestamp + self.get_frequency_is_s() -
+                   datetime.timestamp(datetime.now()))))
+        # l'effettiva simulazione (dati generati real time e mandati a kakfa singolarmente) parte poco dopo
 
     def simulate(self) -> None:
+        self.insert_not_real_time_data()  # strettamente per il poc.
+
         while super().continue_simulating():
 
             hours = (datetime.timestamp(datetime.now()) % 86400) / 3600
