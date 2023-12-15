af6cd18d0b90605a81e5be155d96434a476684df
Dovid308 <giacomodovidio@gmail.com>
2023-12-15 11:27:22 +0100

    fix

--- InitDB/temperature.sql
+++ InitDB/temperature.sql
@@ -45,7 +45,7 @@ FROM innovacity.temperatures
 GROUP BY (timestamp1m, nome_sensore, latitude, longitude); 
 
 
--- tabella per il calcolo della moving avarage. 
+-- tabella per il calcolo della moving avarage.
 CREATE TABLE innovacity.moving_average_calculated_general_temperatures
 (
     moving_avg_temperature Float32,
@@ -63,59 +63,5 @@ SELECT DISTINCT
     FROM innovacity.temperatures
     ORDER BY timestamp1m; 
 --------------------------------------------------------------------------------------------------------------------------------
-
-
-CREATE TABLE innovacity.rain_queue (
-    timestamp DATETIME64,
-    value Float32,
-    type String,
-    latitude Float64,
-    longitude Float64,
-    nome_sensore String
-) ENGINE = Kafka('kafka:9092', 'rain', 'ch_group_1', 'JSONEachRow');
-
-
-CREATE TABLE innovacity.rain (
-    timestamp DATETIME64,
-    value Float32,
-    type String,
-    latitude Float64,
-    longitude Float64,
-    nome_sensore String
-) ENGINE = MergeTree()
-ORDER BY (nome_sensore, timestamp);
-
-
-CREATE MATERIALIZED VIEW rain_consumer_mv TO innovacity.rain
-AS SELECT * FROM innovacity.rain_queue;
-
-
-CREATE TABLE innovacity.rain1m
-(
-    nome_sensore String,
-    timestamp1m DATETIME64,
-    avgRain AggregateFunction(avgState, Float32), -- Using sumState for stateful aggregation
-    latitude Float64,
-    longitude Float64
-)
-ENGINE = AggregatingMergeTree
-ORDER BY (timestamp1m, nome_sensore, longitude, latitude);
-
-
-CREATE MATERIALIZED VIEW innovacity.rain1m_mv
-TO innovacity.rain1m
-AS
-SELECT
-    toStartOfMinute(timestamp) AS timestamp1m,
-    nome_sensore,
-    avgState(value) as avgRain,
-    latitude,
-    longitude
-FROM innovacity.rain_queue
-GROUP BY (timestamp1m, nome_sensore, latitude, longitude);
-
-
-
-
 
 
