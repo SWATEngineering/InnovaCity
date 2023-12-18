8409a7f9398fd6b6e28891cb2d5b17a7c2051b7c
lonewalker0 <riccardoalberto.costantin@studenti.unipd.it>
2023-12-18 11:23:42 +0100

    tabella movingaverage

--- InitDB/temperature.sql
+++ InitDB/temperature.sql
@@ -46,14 +46,28 @@ GROUP BY (timestamp1m, nome_sensore, latitude, longitude);
 
 
 
+-- Creare la tabella aggregata
 CREATE TABLE innovacity.temperatures_ma (
     nome_sensore String,
     timestamp1m DATETIME64,
-    avgTemperature AggregateFunction(avgState, Float32),
+    avgTemperature Float32,
     latitude Float64,
     longitude Float64
-) ENGINE = AggregatingMergeTree
-ORDER BY (timestamp1m, nome_sensore, longitude, latitude);
+) ENGINE = MergeTree()
+ORDER BY (timestamp1m, nome_sensore, latitude, longitude);
+
+-- Creare una Materialized View
+CREATE MATERIALIZED VIEW innovacity.temperatures1m_mov_avg
+TO innovacity.temperatures_ma
+AS
+SELECT
+    nome_sensore,
+    toStartOfMinute(timestamp) AS timestamp1m,
+    avg(value) OVER (PARTITION BY nome_sensore, toStartOfMinute(timestamp) ORDER BY timestamp ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS avgTemperature,
+    latitude,
+    longitude
+FROM innovacity.temperatures;
+
 
 
 
