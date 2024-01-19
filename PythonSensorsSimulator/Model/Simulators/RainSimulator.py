6e4db967bbd75a5340d43eed61ade7ec93d25ee5
ranghetto <rango.matteo@icloud.com>
2024-01-19 09:58:28 +0100

    Rimossi getter e setter inutili da RainSimulator

--- PythonSensorsSimulator/Model/Simulators/RainSimulator.py
+++ PythonSensorsSimulator/Model/Simulators/RainSimulator.py
@@ -29,34 +29,16 @@ class RainSimulator(Simulator):
         self.__rain_duration = 0
         self.__second_rain_left = 0
 
-    def set_rain_intensity(self, intensity: int) -> None:
-        self.__rain_intensity = intensity
-
-    def get_rain_intensity(self) -> int:
-        return self.__rain_intensity
-
-    def set_rain_duration(self, duration: int) -> None:
-        self.__rain_duration = duration
-
-    def get_rain_duration(self) -> int:
-        return self.__rain_duration
-
-    def set_second_rain_left(self, second_left: int) -> None:
-        self.__second_rain_left = second_left
-
-    def get_second_rain_left(self) -> int:
-        return self.__second_rain_left
-
     def try_initiate_rain(self):
         if random.random() < 1 / (3 * 3600 / self.get_frequency_in_s()):
-            self.set_rain_intensity(random.randint(1, 5))
-            self.set_rain_duration(random.randint(7200, 14000))
-            self.set_second_rain_left(self.get_rain_duration())
+            self.__rain_intensity = random.randint(1, 5)
+            self.__rain_duration = random.randint(7200, 14000)
+            self.__second_rain_left = self.__rain_duration
 
     def stop_rain(self):
-        self.set_rain_intensity(0)
-        self.set_rain_duration(0)
-        self.set_second_rain_left(0)
+        self.__rain_intensity = 0
+        self.__rain_duration = 0
+        self.__second_rain_left = 0
 
     def insert_not_real_time_data(self) -> None:
 
@@ -68,22 +50,21 @@ class RainSimulator(Simulator):
         data_to_insert = []
 
         while iter_timestamp > first_timestamp:
-            if self.get_rain_intensity() == 0:
+            if self.__rain_intensity == 0:
                 self.try_initiate_rain()
 
             dato = {
                 "timestamp": str(datetime.fromtimestamp(iter_timestamp)),
                 "value": "{:.2f}".format(
-                    generate_value(self.get_rain_intensity(), self.get_rain_duration(), self.get_second_rain_left())),
+                    generate_value(self.__rain_intensity, self.__rain_duration, self.__second_rain_left)),
                 "type": "RainSimulator",
                 "latitude": super().get_latitude(),
                 "longitude": super().get_longitude(),
                 "nome_sensore": super().get_sensor_name()
             }
 
-            self.set_second_rain_left(
-                self.get_second_rain_left() - super().get_frequency_in_s())
-            if self.get_second_rain_left() == 1:
+            self.__second_rain_left = self.__second_rain_left - super().get_frequency_in_s()
+            if self.__second_rain_left == 1:
                 self.stop_rain()
 
             data_to_insert.append(dato)
@@ -100,22 +81,21 @@ class RainSimulator(Simulator):
     def simulate(self) -> None:
         self.insert_not_real_time_data()
         while super().continue_simulating():
-            if self.get_rain_intensity() == 0:
+            if self.__rain_intensity == 0:
                 self.try_initiate_rain()
 
             dato = {
                 "timestamp": str(datetime.now()),
                 "value": "{:.2f}".format(
-                    generate_value(self.get_rain_intensity(), self.get_rain_duration(), self.get_second_rain_left())),
+                    generate_value(self.__rain_intensity, self.__rain_duration, self.__second_rain_left)),
                 "type": "RainSimulator",
                 "latitude": super().get_latitude(),
                 "longitude": super().get_longitude(),
                 "nome_sensore": super().get_sensor_name()
             }
 
-            self.set_second_rain_left(
-                self.get_second_rain_left() - super().get_frequency_in_s())
-            if self.get_second_rain_left() == 1:
+            self.__second_rain_left = self.__second_rain_left - super().get_frequency_in_s()
+            if self.__second_rain_left() == 1:
                 self.stop_rain()
 
             super().get_writer().write(json.dumps(dato))
