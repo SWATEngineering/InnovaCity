c85e25ab36108d15c4816fc150eb8bbd8ce6a049
lonewalker0 <riccardoalberto.costantin@studenti.unipd.it>
2024-03-06 20:04:18 +0100

    aggiunto sensore umidità e tabella sql

---
+++ InitDB/humidity.sql
@@ -0,0 +1,80 @@
+CREATE TABLE innovacity.humidity_queue (
+    timestamp DATETIME64,
+    value Float32,
+    type String,
+    latitude Float64,
+    longitude Float64,
+    nome_sensore String
+) ENGINE = Kafka('kafka:9092', 'humidity', 'ch_group_1', 'JSONEachRow');
+
+CREATE TABLE innovacity.humidity (
+    timestamp DATETIME64,
+    value Float32,
+    type String,
+    latitude Float64,
+    longitude Float64,
+    nome_sensore String
+) ENGINE = MergeTree()
+ORDER BY (nome_sensore, timestamp);
+
+CREATE MATERIALIZED VIEW humidity_consumer_mv TO innovacity.humidity
+AS SELECT * FROM innovacity.humidity_queue;
+
+
+CREATE TABLE innovacity.humidity1m
+(
+    nome_sensore String,
+    timestamp1m DATETIME64,
+    avgHumidity AggregateFunction(avgState, Float32), 
+    latitude Float64,
+    longitude Float64
+)
+ENGINE = AggregatingMergeTree
+ORDER BY (timestamp1m, nome_sensore, longitude, latitude);
+
+CREATE MATERIALIZED VIEW innovacity.humidity1m_mv
+TO innovacity.humidity1m
+AS
+SELECT
+    toStartOfMinute(timestamp) AS timestamp1m,
+    nome_sensore,
+    avgState(value) as avgHumidity,
+    latitude,
+    longitude
+FROM innovacity.humidity
+GROUP BY (timestamp1m, nome_sensore, latitude, longitude); 
+
+
+/*
+-- Creare la tabella aggregata
+CREATE TABLE innovacity.humidity_ma (
+    nome_sensore String,
+    timestamp1m DATETIME64,
+    avgHumidity Float32,
+    latitude Float64,
+    longitude Float64
+) ENGINE = MergeTree()
+ORDER BY (timestamp1m, nome_sensore, latitude, longitude);
+
+-- Creare una Materialized View
+CREATE MATERIALIZED VIEW innovacity.humidity1m_mov_avg
+TO innovacity.humidity_ma
+AS
+SELECT
+    nome_sensore,
+    toStartOfMinute(timestamp) AS timestamp1m,
+    avg(value) OVER (PARTITION BY  toStartOfMinute(timestamp) ORDER BY timestamp1m ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS avgHumidity,
+    latitude,
+    longitude
+FROM innovacity.humidity;
+*/
+
+
+
+
+
+
+   
+
+
+
--- PythonSensorsSimulator/src/simulator/humidity_sensor_simulator.py
+++ PythonSensorsSimulator/src/simulator/humidity_sensor_simulator.py
@@ -1,6 +1,54 @@
 from sensor_simulator_strategy import SensorSimulatorStrategy
-
+import math
+from ..utils.sensor_types import SensorTypes
+from  ..utils.json_message_maker import json_message_maker
+import time
 
 class HumiditySensorSensorSimulator(SensorSimulatorStrategy):
+    
+     
+    __phase: float=0.2  # Imposta lo spostamento di fase per allineare il picco di umidità con le ore del mattino
+    __amplitude: float
+    __percen: float
+    
+    def __init__(self):
+        super().__init__()
+        self.__amplitude=self._calculate_ampiezza()
+    
+    def _calculate_ampiezza(self):
+        
+            
+        current_month = self._datetime_obj.now().month
+        if 3 <= current_month <= 5:  # Primavera: da marzo a maggio
+                return 0.45
+        elif 6 <= current_month <= 8:  # Estate: da giugno a agosto
+                return 0.3
+        elif 9 <= current_month <= 11:  # Autunno: da settembre a novembre
+                return 0.65
+        else:  # Inverno: da dicembre a febbraio
+                return 0.85
+
+    def _calcola_percentuale(self) -> float:
+        timestamp=self._datetime_obj.now()
+        hours = (timestamp % 86400) / 3600
+        relative_humidity = (math.sin(2 * math.pi * (hours - self.__phase) / 24) * self.__amplitude + 1) / 2
+        relative_humidity += self._random_obj.uniform(-0.01, 0.01)
+        relative_humidity = max(0, min(1, relative_humidity))
+        percentuale = relative_humidity * (100 - 35) + 35
+        return percentuale
+    
     def simulate(self) -> str:
-        return ""
+                      
+        timestamp = self._datetime_obj.now()
+        self.__percen = self._calcola_percentuale()
+    
+        reading = {
+                "type": "%",
+                "value": round(self.__percen,2)
+        }
+    
+        dato = json_message_maker(SensorTypes.HUMIDITY, str(timestamp), reading, self._sensor_name, self._coordinates)
+    
+        time.sleep(self._wait_time_in_seconds.total_seconds())
+    
+        return dato
