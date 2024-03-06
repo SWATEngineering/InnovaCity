e6225d84116777960dc5a8183ddbf3d18d5e0854
kara <ssctonioloriccardo@gmail.com>
2024-03-06 16:11:12 +0100

    Create json_message_maker.py

---
+++ PythonSensorsSimulator/src/utils/json_message_maker.py
@@ -0,0 +1,15 @@
+from utils.sensor_types import SensorTypes
+from utils.coordinates import Coordinates
+
+from typing import List, Dict
+import json
+
+
+def json_message_maker(type: SensorTypes, timestamp: str, readings: List[Dict], name: str, location: Coordinates):
+    return json.dumps({
+        "type": type.value,
+        "timestamp": timestamp,
+        "readings": readings,
+        "name": name,
+        "location": json.loads(location.get_geo_json())
+    })
