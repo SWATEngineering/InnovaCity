ff61f840f93d941765af598822882d50b856d950
kara <ssctonioloriccardo@gmail.com>
2024-03-12 10:02:52 +0100

    Aggiunta tabella per il vento

--- PythonSensorsSimulator/main.py
+++ PythonSensorsSimulator/main.py
@@ -1,6 +1,7 @@
 import os
-# from src.simulator_utils.kafka_simulator_executor_factory import KafkaSimulatorExecutorFactory
-from src.simulator_utils.stdout_simulator_executor_factory import StdoutSimulatorExecutorFactory
+from src.simulator_utils.kafka_simulator_executor_factory import KafkaSimulatorExecutorFactory
+
+# from src.simulator_utils.stdout_simulator_executor_factory import StdoutSimulatorExecutorFactory
 
 # env var reading
 KAFKA_HOST = os.environ.get("KAFKA_HOST", "kafka")
@@ -12,16 +13,16 @@ config_str = config_file.read()
 config_file.close()
 
 # configuration factory creation (kafka writer)
-# sim_exe_factory = KafkaSimulatorExecutorFactory(
-   # configs=config_str,
-   # data_broker_host=KAFKA_HOST,
-   # data_broker_port=KAFKA_PORT
-# )
+sim_exe_factory = KafkaSimulatorExecutorFactory(
+    configs=config_str,
+    data_broker_host=KAFKA_HOST,
+    data_broker_port=KAFKA_PORT
+)
 
 # configuration factory creation (stdout writer)
-sim_exe_factory = StdoutSimulatorExecutorFactory(
-    configs=config_str
-)
+# sim_exe_factory = StdoutSimulatorExecutorFactory(
+#   configs=config_str
+# )
 
 # simulator executor creation
 sim_exe = sim_exe_factory.create()
