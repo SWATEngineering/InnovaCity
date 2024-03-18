b93bd509f62e9b8ed9e7a3f987ce93fb373f5626
kara <ssctonioloriccardo@gmail.com>
2024-03-18 11:21:54 +0100

    test per il formato e la consistenza dei valori per il sensore di temperatura.

--- PythonSensorsSimulator/Model/Writers/Writer.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Writer(ABC):
-
-    @abstractmethod
-    def write(self, to_write: str) -> None:
-        pass
--- PythonSensorsSimulator/main.py
+++ PythonSensorsSimulator/main.py
@@ -1,5 +1,6 @@
 import os
-from src.simulator_utils.kafka_simulator_executor_factory import KafkaSimulatorExecutorFactory
+#from src.simulator_utils.kafka_simulator_executor_factory import KafkaSimulatorExecutorFactory
+from src.simulator_utils.stdout_simulator_executor_factory import StdoutSimulatorExecutorFactory
 
 # env var reading
 KAFKA_HOST = os.environ.get("KAFKA_HOST", "kafka")
@@ -11,16 +12,16 @@ config_str = config_file.read()
 config_file.close()
 
 # configuration factory creation (kafka writer)
-sim_exe_factory = KafkaSimulatorExecutorFactory(
-    configs=config_str,
-    data_broker_host=KAFKA_HOST,
-    data_broker_port=KAFKA_PORT
-)
+#sim_exe_factory = KafkaSimulatorExecutorFactory(
+#    configs=config_str,
+#    data_broker_host=KAFKA_HOST,
+#    data_broker_port=KAFKA_PORT
+#)
 
 # configuration factory creation (stdout writer)
-# sim_exe_factory = StdoutSimulatorExecutorFactory(
-#   configs=config_str
-# )
+sim_exe_factory = StdoutSimulatorExecutorFactory(
+  configs=config_str
+)
 
 # simulator executor creation
 sim_exe = sim_exe_factory.create()
