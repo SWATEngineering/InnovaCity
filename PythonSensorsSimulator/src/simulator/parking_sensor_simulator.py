7446a31a39a63bec9ffcbf44add0d70d87cced4e
Matteo Rango <rango.matteo@icloud.com>
2024-03-28 12:50:57 +0100

    Add parking tests and fix dashboard (#97)
    Co-authored-by: kara <81550667+RiccardoTonioloDev@users.noreply.github.com>

--- PythonSensorsSimulator/src/simulator/parking_sensor_simulator.py
+++ PythonSensorsSimulator/src/simulator/parking_sensor_simulator.py
@@ -6,49 +6,50 @@ from typing import Type
 from src.simulator.sensor_simulator_strategy import SensorSimulatorStrategy
 from src.utils.json_message_maker import json_message_maker
 from src.utils.sensor_types import SensorTypes
+from dataclasses import dataclass
+
+
+@dataclass
+class ParkingGaussianPeak:
+    mean_position_in_minutes: int
+    std_dev_in_minutes: int
+    weight: float
 
 
 class ParkingSensorSimulator(SensorSimulatorStrategy):
+    __peak1: ParkingGaussianPeak
+    __peak2: ParkingGaussianPeak
     __max_cars: int
-    __peak1_mean_minutes: int
-    __peak1_weight: float
-    __peak1_std_dev_minutes: int
-    __peak2_mean_minutes: int
-    __peak2_weight: float
-    __peak2_std_dev_minutes: int
     __total_minutes_in_day: int
     __noise_scale: float
     __daily_data: np.ndarray
 
     def __init__(self, sensor_name: str, random_obj: Random, datetime_obj: Type[datetime], coordinates: Coordinates):
         super().__init__(sensor_name, random_obj, datetime_obj, coordinates)
+        self.__peak1 = ParkingGaussianPeak(12 * 60, 120, 0.6)
+        self.__peak2 = ParkingGaussianPeak(21 * 60, 30, 0.3)
         self.__max_cars = 100
-        self.__peak1_mean_minutes = 12 * 60
-        self.__peak1_weight = 0.6
-        self.__peak1_std_dev_minutes = 120
-        self.__peak2_mean_minutes = 21 * 60
-        self.__peak2_weight = 0.3
-        self.__peak2_std_dev_minutes = 30
         self.__total_minutes_in_day = 24 * 60
-        self.__noise_scale = 0.01
+        self.__noise_scale = 0.03
         self.__daily_data = self.__generate_dual_peak_gaussian()
+
 
     def __generate_dual_peak_gaussian(self) -> np.ndarray:
         minutes = np.arange(self.__total_minutes_in_day)
-        peak1 = (self.__peak1_weight *
-                 np.exp(-((minutes - self.__peak1_mean_minutes) / self.__peak1_std_dev_minutes) ** 2))
-        peak2 = (self.__peak2_weight *
-                 np.exp(-((minutes - self.__peak2_mean_minutes) / self.__peak2_std_dev_minutes) ** 2))
+        peak1_vals: list = (self.__peak1.weight *
+                      np.exp(
+                          -((minutes - self.__peak1.mean_position_in_minutes) / self.__peak1.std_dev_in_minutes) ** 2))
+        peak2_vals: list = (self.__peak2.weight *
+                      np.exp(
+                          -((minutes - self.__peak2.mean_position_in_minutes) / self.__peak2.std_dev_in_minutes) ** 2))
 
         # Adding random noise to the peaks
-        peak1 += self.__noise_scale * \
-            np.random.normal(0, 1, self.__total_minutes_in_day)
-        peak1[peak1 < 0] = 0  # Set negative values to 0
-        peak2 += self.__noise_scale * \
-            np.random.normal(0, 1, self.__total_minutes_in_day)
-        peak2[peak2 < 0] = 0  # Set negative values to 0
+        peak1_vals = [x + self._random_obj.normalvariate(0, 1) * self.__noise_scale for x in peak1_vals]
+        peak1_vals = [0 if x < 0 else x for x in peak1_vals]  # Set negative values to 0
+        peak2_vals = [x + self._random_obj.normalvariate(0, 1) * self.__noise_scale for x in peak2_vals]
+        peak2_vals = [0 if x < 0 else x for x in peak2_vals]  # Set negative values to 0
 
-        total_cars = peak1 + peak2
+        total_cars = peak1_vals + peak2_vals
         scaled_cars = total_cars / np.max(total_cars) * self.__max_cars
         return scaled_cars
 
@@ -74,7 +75,7 @@ class ParkingSensorSimulator(SensorSimulatorStrategy):
         # Send how many cars are there in the parking
         reading1 = {
             "type": "Number",
-            "value": self.__daily_data[minutes_from_midnight]
+            "value": round(self.__daily_data[minutes_from_midnight])
         }
         # Send how many parking lots it has
         reading2 = {
